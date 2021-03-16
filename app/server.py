ddfb92b464be07b88b74e6a06e8c5cbc025b700d
Jeswin Jose <shield178@gmail.com>
2021-03-16 15:47:14 +0800

    updated back to.pkl file

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/file/d/1-9WQcRNpNcuLSU5SZ0GBldJGGgRy2InL/view?usp=sharing'
-export_file_name = 'export.pkl'
+export_file_url = 'https://www.dropbox.com/s/imxe5dysajp41db/trained_model2.pkl?dl=0'
+export_file_name = 'trained_model2.pkl'
 
 classes = ['daisy', 'dandelion', 'rose', 'sunflower', 'tulip']
 path = Path(__file__).parent
